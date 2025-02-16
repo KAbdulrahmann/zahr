45a8793a487f0a5a415d14c7104f5d88f87dc96c
khaled <khaledabdo7777@gmail.com>
2025-02-16 12:27:54 +0200

    first commit

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cors({ origin: "*" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors());
+// app.use(cors());
 
 
 /* ROUTES */
