6872db06b0f2edf6bc298aec2f3985edafdddba0
khaled <khaledabdo7777@gmail.com>
2025-02-16 23:54:08 +0200

    first commit

--- index.js
+++ index.js
@@ -28,15 +28,14 @@ import {
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
-app.use(cors({ origin: "*" }));
+// app.use(helmet());
+// app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+
 
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use(cors());
-
+app.use(cors());
 
 /* ROUTES */
 app.use("/client", clientRoutes);
